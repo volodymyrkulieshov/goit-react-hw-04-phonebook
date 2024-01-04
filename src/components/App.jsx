79c4bf44c4d3883536e6dd161901d924c7151233
Volodymyr <vovkul@gmail.com>
2024-01-04 22:25:30 +0100

    add config and start files(src)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,86 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+import { nanoid } from 'nanoid';
+import css from './App.module.css';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+  componentDidMount = () => {
+    const contacts = localStorage.getItem('contacts');
+    if (JSON.parse(contacts)) {
+      this.setState({ contacts: JSON.parse(contacts) });
+    }
+  };
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+  addContact = contact => {
+    const isInContacts = this.state.contacts.some(
+      ({ name }) => name.toLowerCase() === contact.name.toLowerCase()
+    );
+    if (isInContacts) {
+      alert(`${contact.name} is already in contacts`);
+      return;
+    }
+    this.setState(prevState => ({
+      contacts: [{ id: nanoid(), ...contact }, ...prevState.contacts],
+    }));
+  };
+
+  changeFilter = event => {
+    this.setState({ filter: event.target.value });
+  };
+
+  FilteredContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  removeContact = contactId => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(({ id }) => id !== contactId),
+      };
+    });
+  };
+
+  render() {
+    const visibleContacts = this.FilteredContacts();
+    const { filter } = this.state;
+    return (
+      <div className={css.container}>
+        <h1 className={css.title}>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact} />
+        <h2 className={css.subTitle}>Contacts</h2>
+
+        {this.state.contacts.length > 0 && (
+          <Filter value={filter} changeFilter={this.changeFilter} />
+        )}
+        {this.state.contacts.length > 0 && (
+          <ContactList
+            contacts={visibleContacts}
+            onRemoveContact={this.removeContact}
+          />
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,60 @@
+import { Component } from 'react';
+import css from './ContactForm.module.css';
+
+class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+  handleChange = ({ target: { value, name } }) => {
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    this.props.onSubmit({ name: this.state.name, number: this.state.number });
+
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ number: '', name: '' });
+  };
+
+  render() {
+    return (
+      <form className={css.contactForm} onSubmit={this.handleSubmit}>
+        <label className={css.label} htmlFor="example">
+          Name
+          <input
+            className={css.input}
+            type="text"
+            name="name"
+            value={this.state.name}
+            onChange={this.handleChange}
+            required
+          />
+        </label>
+
+        <label className={css.label} htmlFor="example">
+          Number
+          <input
+            className={css.input}
+            type="tel"
+            name="number"
+            value={this.state.number}
+            onChange={this.handleChange}
+            required
+          />
+        </label>
+
+        <button className={css.button} type="submit">
+          Add contact{' '}
+        </button>
+      </form>
+    );
+  }
+}
+
+export default ContactForm;
